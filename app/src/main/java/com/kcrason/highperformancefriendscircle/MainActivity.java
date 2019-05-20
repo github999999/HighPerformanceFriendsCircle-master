5f37d9885a178ca5a075798577541e46ecc64cc0
wangfeigit <406981543>
2019-05-20 17:52:19 +0800

    Initial commit

---
+++ app/src/main/java/com/kcrason/highperformancefriendscircle/MainActivity.java
@@ -0,0 +1,163 @@
+package com.kcrason.highperformancefriendscircle;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.kcrason.highperformancefriendscircle.adapters.FriendCircleAdapter;
+import com.kcrason.highperformancefriendscircle.beans.FriendCircleBean;
+import com.kcrason.highperformancefriendscircle.interfaces.OnPraiseOrCommentClickListener;
+import com.kcrason.highperformancefriendscircle.others.DataCenter;
+import com.kcrason.highperformancefriendscircle.others.FriendsCircleAdapterDivideLine;
+import com.kcrason.highperformancefriendscircle.others.GlideSimpleTarget;
+import com.kcrason.highperformancefriendscircle.utils.Utils;
+import com.kcrason.highperformancefriendscircle.widgets.EmojiPanelView;
+import java.util.List;
+import ch.ielse.view.imagewatcher.ImageWatcher;
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleObserver;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,
+        OnPraiseOrCommentClickListener, ImageWatcher.OnPictureLongPressListener, ImageWatcher.Loader {
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private Disposable mDisposable;
+    private FriendCircleAdapter mFriendCircleAdapter;  // 列表适配器
+    private ImageWatcher mImageWatcher;
+    private EmojiPanelView mEmojiPanelView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEmojiPanelView = findViewById(R.id.emoji_panel_view);
+        mEmojiPanelView.initEmojiPanel(DataCenter.emojiDataSources);
+        mSwipeRefreshLayout = findViewById(R.id.swpie_refresh_layout);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);  // 控件列表
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+//        findViewById(R.id.img_back).setOnClickListener(v ->
+//                startActivity(new Intent(MainActivity.this, EmojiPanelActivity.class)));
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    Glide.with(MainActivity.this).resumeRequests();
+                } else {
+                    Glide.with(MainActivity.this).pauseRequests();
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+        mImageWatcher = findViewById(R.id.image_watcher);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new FriendsCircleAdapterDivideLine());
+        mFriendCircleAdapter = new FriendCircleAdapter(this, recyclerView, mImageWatcher);
+        recyclerView.setAdapter(mFriendCircleAdapter);
+        mImageWatcher.setTranslucentStatus(Utils.calcStatusBarHeight(this));
+        mImageWatcher.setErrorImageRes(R.mipmap.error_picture);
+        mImageWatcher.setOnPictureLongPressListener(this);
+        mImageWatcher.setLoader(this);
+        Utils.showSwipeRefreshLayout(mSwipeRefreshLayout, this::asyncMakeData);
+    }
+
+
+    // 异步加载数据
+    private void asyncMakeData() {
+//        mDisposable = Single.create((SingleOnSubscribe<List<FriendCircleBean>>) emitter ->
+//                emitter.onSuccess(DataCenter.makeFriendCircleBeans(this)))
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe((friendCircleBeans, throwable) -> {
+//                    Utils.hideSwipeRefreshLayout(mSwipeRefreshLayout);
+//                    if (friendCircleBeans != null && throwable == null) {
+//                        mFriendCircleAdapter.setFriendCircleBeans(friendCircleBeans);
+//                    }
+//                });
+
+        mDisposable = Single.create(new SingleOnSubscribe<List<FriendCircleBean>>() {
+            @Override
+            public void subscribe(SingleEmitter<List<FriendCircleBean>> emitter) throws Exception {
+                // 需要异步加载的数据在io流中执行
+                emitter.onSuccess(DataCenter.makeFriendCircleBeans(MainActivity.this));
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<FriendCircleBean>>() {
+                    @Override
+                    public void accept(List<FriendCircleBean> friendCircleBeans) throws Exception {
+                        Utils.hideSwipeRefreshLayout(mSwipeRefreshLayout);
+                        if (friendCircleBeans != null) {
+                            mFriendCircleAdapter.setFriendCircleBeans(friendCircleBeans);
+                        }
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mDisposable != null && !mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        asyncMakeData();
+    }
+
+    @Override
+    public void onPraiseClick(int position) {
+        Toast.makeText(this, "You Click Praise!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCommentClick(int position) {
+//        Toast.makeText(this, "you click comment", Toast.LENGTH_SHORT).show();
+        mEmojiPanelView.showEmojiPanel();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mImageWatcher.handleBackPressed()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onPictureLongPress(ImageView v, String url, int pos) {
+
+    }
+
+
+    @Override
+    public void load(Context context, String url, ImageWatcher.LoadCallback lc) {
+        Glide.with(context).asBitmap().load(url).into(new GlideSimpleTarget(lc));
+    }
+}
---
+++ app/src/main/java/com/kcrason/highperformancefriendscircle/interfaces/OnPraiseOrCommentClickListener.java
@@ -0,0 +1,8 @@
+package com.kcrason.highperformancefriendscircle.interfaces;
+
+public interface OnPraiseOrCommentClickListener {
+    void onPraiseClick(int position);
+
+
+    void onCommentClick(int position);
+}
